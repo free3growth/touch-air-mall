a6dffdaeb9d9b45b53afff7d6d968e79fb178c97
wangb1in <1178321785@qq.com>
2020-12-09 09:47:30 +0800

    feat:分布式组件-SpringCloud
    --Gateway网关
    --路由（Route）
    --断言（Predicate）
    --过滤（Filter）
    --网关测试

---
+++ mall-gateway/src/main/java/com/touch/air/mall/gateway/MallGatewayApplication.java
@@ -0,0 +1,24 @@
+package com.touch.air.mall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 网关服务
+ * @author bin.wang
+ *
+ * 1.开启服务注册与发现
+ *    配置nacos的注册中心
+ *
+ */
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class MallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallGatewayApplication.class, args);
+    }
+
+}
